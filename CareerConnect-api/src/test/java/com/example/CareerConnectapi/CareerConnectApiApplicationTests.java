85af53fd842d27694cde2b779f947959876e41e5
eljanagj <gjecbitriqeljana@gmail.com>
2024-03-23 15:32:46 +0100

    Created spring boot app and react app

---
+++ CareerConnect-api/src/test/java/com/example/CareerConnectapi/CareerConnectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CareerConnectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerConnectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
