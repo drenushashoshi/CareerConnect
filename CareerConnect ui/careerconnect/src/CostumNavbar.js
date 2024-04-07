0db4c865206251006f189a1b5c9f5f45c721038f
drenushashoshi <ds68479@ubt-uni.net>
2024-04-07 16:40:57 +0200

    Created Navbar and JobListing Page

---
+++ CareerConnect ui/careerconnect/src/CostumNavbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Navbar, Nav, NavDropdown, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+
+function CostumNavbar() {
+    const navigate = useNavigate();
+
+    const handleProfileButtonClick = () => {
+        // Logic to check if user is logged in
+        // For now imma let it false till so it goes to log in
+        const isLoggedIn = false;
+
+        // Redirect to appropriate page based on login status
+        if (isLoggedIn) {
+            navigate('/profile'); // Redirect to profile page
+        } else {
+            navigate('/'); // Redirect to login page
+        }
+    };
+
+    return (
+        <Navbar expand="lg" bg="white" variant="light" className="shadow sticky-top p-0">
+            <Navbar.Brand href="#" className="d-flex align-items-center text-center py-0 px-4 px-lg-5">
+                <h1 className="m-0 text-primary">CareerConnect</h1>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarCollapse" className="me-4" />
+            <Navbar.Collapse id="navbarCollapse">
+                <Nav className="ms-auto p-4 p-lg-0">
+                    <Nav.Link href="#" className="nav-item nav-link active">Home</Nav.Link>
+                    <Nav.Link href="#" className="nav-item nav-link">About</Nav.Link>
+                    <NavDropdown title="Apliko" id="basic-nav-dropdown" className="nav-item dropdown">
+                        <NavDropdown.Item href="./JobListing">Pune</NavDropdown.Item>
+                        <NavDropdown.Item href="#">Praktike</NavDropdown.Item>
+                        <NavDropdown.Item href="#">Kurse</NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link href="#" className="nav-item nav-link">Posto shpallje</Nav.Link>
+                </Nav>
+                <Button variant="primary" className="rounded-0 py-4 px-lg-5 d-none d-lg-block" onClick={handleProfileButtonClick}>Your Profile<i className="fa fa-arrow-right ms-3"></i></Button>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+}
+
+export default CostumNavbar;
--- CareerConnect ui/careerconnect/src/JobListing.js
+++ CareerConnect ui/careerconnect/src/JobListing.js
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
 import { Card, Button, Modal, Form } from 'react-bootstrap';
 import backgroundImage from './login-test2.avif';
-import './styles.css';
+import CostumNavbar from "./CostumNavbar"; // Importing the Navbar component
 
 function JobListing() {
     const [showModal, setShowModal] = useState(false);
-    const [selectedCategory, setSelectedCategory] = useState('');
-    const [selectedLocation, setSelectedLocation] = useState('');
-    const [searchText, setSearchText] = useState('');
     const locations = [
         'Deçan', 'Dragash', 'Drenas', 'Ferizaj', 'Fushë Kosovë', 'Gjakovë', 'Gjilan', 'Burim',
         'Kaçanik', 'Dardanë', 'Klinë', 'Lipjan', 'Malishevë', 'Mitrovicë', 'Kastriot', 'Pejë',
@@ -15,29 +12,9 @@ function JobListing() {
         'Viti', 'Vushtrri'
     ];
 
-    const handleShowModal = () => setShowModal(true);
-    const handleCloseModal = () => setShowModal(false);
-
     const handleApplyClick = () => {
         // Logic for applying to the job
         console.log('Applied to the job');
-    };
-
-    const handleSearchClick = () => {
-        // Logic for searching jobs based on selectedCategory, selectedLocation, and searchText
-        console.log('Searching jobs in category: ', selectedCategory, ' at location: ', selectedLocation, ' with search text: ', searchText);
-    };
-
-    const handleCategoryChange = (event) => {
-        setSelectedCategory(event.target.value);
-    };
-
-    const handleLocationChange = (event) => {
-        setSelectedLocation(event.target.value);
-    };
-
-    const handleTextChange = (event) => {
-        setSearchText(event.target.value);
     };
 
     const handleWebpageLink = (url) => {
@@ -45,73 +22,67 @@ function JobListing() {
     };
 
     return (
-        <div className='job-listing template d-flex justify-content-center align-items-center 100-w vh-100 bg-primary text-white' style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', opacity: 0.85 }}>
-            <div className='d-flex flex-column align-items-center'>
-                <h1 className="custom-margin"><b>CareerConnect</b></h1>
-                <p className='w-50 mt-3'>
-                    Miresevini ne CareerConnect!
-                </p>
-                <div className='d-flex justify-content-center'>
-                    <Button variant="primary" className="me-2" onClick={() => handleWebpageLink('http://localhost:3000/JobListing')}>Jobs</Button>
-                    <Button variant="primary" className="me-2" onClick={() => handleWebpageLink('https://yourwebsite.com/internships')}>Internships</Button>
-                    <Button variant="primary" onClick={() => handleWebpageLink('https://yourwebsite.com/courses')}>Courses</Button>
-                </div>
-                <div className='d-flex justify-content-center w-75 p-3 bg-transparent'>
-                    <select className="form-select me-2" style={{ width: '150px' }} onChange={handleCategoryChange} value={selectedCategory}>
-                        <option value="">All Categories</option>
-                        <option value="frontend">Frontend</option>
-                        <option value="backend">Backend</option>
-                        <option value="fullstack">Fullstack</option>
-                        {/* Add more options as needed */}
-                    </select>
-                    <select className="form-select me-2" style={{ width: '150px' }} onChange={handleLocationChange} value={selectedLocation}>
-                        <option value="">All Locations</option>
-                        {locations.map(location => (
-                            <option key={location} value={location}>{location}</option>
-                        ))}
-                    </select>
-                    <Form.Control type="text" placeholder="Search Text" className="me-2" style={{ width: '150px' }} value={searchText} onChange={handleTextChange} />
-                    <Button variant="primary" onClick={handleSearchClick}>Search Jobs</Button>
-                </div>
-                <div className='w-75 p-3 bg-transparent'>
-                    <Card className="mb-4">
-                        <Card.Body>
-                            <Card.Title>Frontend Developer</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">Tech Co.</Card.Subtitle>
-                            <Card.Text>
-                                We are looking for a talented Frontend Developer to join our team.
-                                The ideal candidate should have experience with HTML, CSS, JavaScript, and React.js.
-                            </Card.Text>
-                            <Button variant="primary" onClick={handleApplyClick}>Apply</Button>
-                        </Card.Body>
-                    </Card>
-                    <Card className="mb-4">
-                        <Card.Body>
-                            <Card.Title>Backend Developer</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">Soft Co.</Card.Subtitle>
-                            <Card.Text>
-                                Soft Co. is seeking a skilled Backend Developer proficient in Node.js,
-                                MongoDB, and RESTful APIs to join our backend team.
-                            </Card.Text>
-                            <Button variant="primary" onClick={handleApplyClick}>Apply</Button>
-                        </Card.Body>
-                    </Card>
-                </div>
-                <div className="mt-3">
-                    <Button variant="primary" onClick={handleShowModal} style={{ backgroundColor: '#0047AB' }}>Post a Job</Button>
+        <>
+            <CostumNavbar />
+            {/* Search Start */}
+            <div className="container-fluid bg-primary py-5">
+                <div className="container">
+                    <div className="row g-3 justify-content-center align-items-center">
+                        <div className="col-md-3">
+                            <input type="text" className="form-control" placeholder="Keyword" />
+                        </div>
+                        <div className="col-md-3">
+                            <select className="form-select">
+                                    <option value="">Industria</option>
+                                    <option value="administrata">Administratë</option>
+                                    <option value="arkitekture">Arkitekturë</option>
+                                    <option value="art_dhe_kulture">Art dhe Kulturë</option>
+                                    <option value="banka">Banka</option>
+                                    <option value="industria_automobilistike">Industria Automobilistike</option>
+                                    <option value="retail_dhe_distribuim">Retail dhe Distribuim</option>
+                                    <option value="ndertimtari_patundshmeri">Ndërtimtari & Patundshmëri</option>
+                                    <option value="mbeshtetje_konsumatoreve_call_center">Mbështetje e Konsumatorëve, Call Center</option>
+                                    <option value="ekonomi_financ_kontabilitet">Ekonomi, Financë, Kontabilitet</option>
+                                    <option value="edukim_shkence_hulumtim">Edukim, Shkencë & Hulumtim</option>
+                                    <option value="pune_te_pergjithshme">Punë të Përgjithshme</option>
+                                    <option value="burime_njerzore">Burime Njerëzore</option>
+                                    <option value="teknologji_informacionit">Teknologji e Informacionit</option>
+                                    <option value="gazetari_shtyp_media">Gazetari, Shtyp & Media</option>
+                                    <option value="ligj_legjislacion">Ligj & Legjislacion</option>
+                                    <option value="menaxhment">Menaxhment</option>
+                                    <option value="marketing_reklamim_pr">Marketing, Reklamim & PR</option>
+                                    <option value="inxhinieri">Inxhinieri</option>
+                                    <option value="shendetesi_medicin">Shëndetësi, Medicinë</option>
+                                    <option value="industri_farmaceutike">Industri Farmaceutike</option>
+                                    <option value="prodhim">Prodhim</option>
+                                    <option value="sherbime_publike_qeveritare">Shërbime Publike, Qeveritare</option>
+                                    <option value="siguri_mbrojtje">Siguri & Mbrojtje</option>
+                                    <option value="industri_sherbimit">Industri të Shërbimit</option>
+                                    <option value="telekomunikim">Telekomunikim</option>
+                                    <option value="tekstil_lekure_veshembathje">Tekstil, Lëkurë, Industri Veshëmbathjeje</option>
+                                    <option value="menaxhment_ekzekutiv">Menaxhment Ekzekutiv</option>
+                                    <option value="gastronomi_hoteleri_turizem">Gastronomi, Hoteleri, Turizëm</option>
+                                    <option value="perkthim_interpretim">Përkthim, Interpretim</option>
+                                    <option value="transport_logjistike">Transport, Logjistikë</option>
+                                    <option value="industri_perpunimit_drurit">Industri e Përpunimit të Drurit</option>
+                            </select>
+                        </div>
+                        <div className="col-md-3">
+                            <select className="form-select">
+                                <option selected>Location</option>
+                                {locations.map((location, index) => (
+                                    <option key={index} value={location}>{location}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="col-md-3">
+                            <Button variant="dark" className="w-100" onClick={() => handleApplyClick()}>Search</Button>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            <Modal show={showModal} onHide={handleCloseModal} centered>
-                <Modal.Body className='text-center custom-font'>
-                    <h5 className='mt-3'>Are you a company looking to hire?</h5>
-                    <div className='mt-4 mb-4'>
-                        {/* Link to the page where you can post a job */}
-                        <Button variant="primary" onClick={handleCloseModal}>Post a Job</Button>
-                    </div>
-                </Modal.Body>
-            </Modal>
-        </div>
+            {/* Search End */}
+        </>
     );
 }
 
