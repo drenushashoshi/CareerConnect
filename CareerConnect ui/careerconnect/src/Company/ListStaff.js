db812620ca376db74a13957c1ad49f551e83cce6
eljanagj <gjecbitriqeljana@gmail.com>
2024-04-22 15:18:12 +0200

    Edit Company`s Staff functionality

--- CareerConnect ui/careerconnect/src/Company/ListStaff.js
+++ CareerConnect ui/careerconnect/src/Company/ListStaff.js
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import { listCompanyStaffs } from '../Services/CompanyStaffService';
+import { listCompanyStaffs, getCompanyStaff, updateCompanyStaff } from '../Services/CompanyStaffService';
 import {
     MDBRow,
     MDBCard,
     MDBCardBody,
-    MDBCardText
+    MDBCardText,
+    MDBCol,
+    MDBInput,
+    MDBBtn
 } from 'mdb-react-ui-kit';
+import { ReactComponent as EditButton } from './pencil.svg';
+import Modal from 'react-bootstrap/Modal';
 
 const ListStaff = ({ companyId }) => {
     const [companyStaff, setCompanyStaff] = useState([]);
+    const [companyStaffId, setCompanyStaffId] = useState(null);
+    const [name, setName] = useState('');
+    const [surname, setSurname] = useState('');
+    const [role, setRole] = useState('');
+    const [showModal, setShowModal] = useState(false);
 
     useEffect(() => {
         listCompanyStaffs(companyId)
@@ -20,22 +30,118 @@ const ListStaff = ({ companyId }) => {
             });
     }, [companyId]);
 
+    const handleShowModal = (id) => {
+        setShowModal(true);
+        setCompanyStaffId(id);
+        getCompanyStaff(id)
+            .then((response) => {
+                const { name, surname, role } = response.data;
+                setName(name);
+                setSurname(surname);
+                setRole(role);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+        setCompanyStaffId(null);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const staff = { name, surname, role };
+        if (companyStaffId) {
+            updateCompanyStaff(companyStaffId, staff)
+                .then((response) => {
+                    console.log(response.data);
+                    handleCloseModal();
+                    window.location.href = `/CompanyPage/${companyId}`;
+                })
+                .catch(error => {
+                    console.error("Error updating companyStaff:", error);
+                });
+        }
+    };
+
     return (
         <div className="container">
             <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-                {companyStaff.map(companyStaff => (
-                    <div key={companyStaff.id} className="col mb-4">
-                        <MDBCard className="h-100" style={{ backgroundColor: '#e3f2fd', boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.1)' }}>
+                {companyStaff.map(staff => (
+                    <div key={staff.id} className="col mb-4">
+                        <MDBCard className="h-100 position-relative" style={{ backgroundColor: '#e3f2fd', boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.1)' }}>
                             <MDBCardBody className="text-center">
+                                <div className="position-absolute top-0 end-0 mt-2 me-2">
+                                <button
+                                    onClick={() => handleShowModal(staff.id)}
+                                    style={{
+                                        border: 'none',
+                                        background: 'none',
+                                        padding: 0,
+                                        cursor: 'pointer' 
+                                    }}
+                                >
+                                    <EditButton />
+                                </button>
+                                </div>
                                 <MDBRow>
-                                    <MDBCardText>{companyStaff.name} {companyStaff.surname}</MDBCardText>
+                                    <MDBCardText>{staff.name} {staff.surname}</MDBCardText>
                                 </MDBRow>
-                                <MDBCardText>{companyStaff.role}</MDBCardText>
+                                <MDBCardText>{staff.role}</MDBCardText>
                             </MDBCardBody>
                         </MDBCard>
                     </div>
                 ))}
             </div>
+            <Modal show={showModal} onHide={handleCloseModal} centered>
+                <Modal.Body className='text-center custom-font'>
+                    <MDBCard className="mb-4" style={{ backgroundColor: '#e3f2fd', boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.1)' }}>
+                        <form onSubmit={handleSubmit}>
+                            <MDBCardBody className="text-center">
+                                <MDBRow>
+                                    <MDBCol>
+                                        <MDBInput
+                                            type="text"
+                                            placeholder='Emri'
+                                            value={name}
+                                            onChange={(e) => setName(e.target.value)}
+                                        />
+                                    </MDBCol>
+                                    <MDBCol>
+                                        <MDBInput
+                                            type="text"
+                                            placeholder='Mbiemri'
+                                            value={surname}
+                                            onChange={(e) => setSurname(e.target.value)}
+                                        />
+                                    </MDBCol>
+                                </MDBRow><br />
+                                <MDBInput
+                                    type="text"
+                                    placeholder='Pozita e punes (p.sh CEO)'
+                                    value={role}
+                                    onChange={(e) => setRole(e.target.value)}
+                                />
+                            </MDBCardBody>
+                            <div className="d-flex justify-content-center">
+                                <MDBBtn
+                                    className='mb-3 btn-primary'
+                                    type="submit"
+                                    style={{
+                                        border: '1px solid #0d6efd',
+                                        width: '140px',
+                                        height: '40px'
+                                    }}
+                                >
+                                    Save Changes
+                                </MDBBtn>
+                            </div>
+                        </form>
+                    </MDBCard>
+                </Modal.Body>
+            </Modal>
         </div>
     );
 }
--- CareerConnect ui/careerconnect/src/Services/CompanyStaffService.js
+++ CareerConnect ui/careerconnect/src/Services/CompanyStaffService.js
@@ -4,3 +4,5 @@ const REST_API_BASE_URL='http://localhost:8080/api/CompanyStaff';
 
 export const createCompanyStaff=(companyStaff)=>axios.post(REST_API_BASE_URL, companyStaff);
 export const listCompanyStaffs = (companyId) => axios.get(`${REST_API_BASE_URL}?companyId=${companyId}`);
+export const getCompanyStaff=(companyStaffId)=>axios.get(REST_API_BASE_URL+'/'+companyStaffId);
+export const updateCompanyStaff=(companyStaffId, companyStaff)=>axios.put(REST_API_BASE_URL +'/'+companyStaffId, companyStaff);
